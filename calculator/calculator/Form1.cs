f9c4e987bcaefb9a18d919643687321492afc057
vadimparemskiy <vadim.paremskiy@mail.ru>
2018-07-04 11:02:49 +0600

    simple calculator

--- calculator/calculator/Form1.cs
+++ calculator/calculator/Form1.cs
@@ -16,5 +16,44 @@ namespace calculator
         {
             InitializeComponent();
         }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string firstValuetext = textBox1.Text;
+            double firstValue = Convert.ToDouble(firstValuetext);
+            string secondValuetext = textBox2.Text;
+            double secondValue = Convert.ToDouble(secondValuetext);
+            double result = secondValue + firstValue;
+            textBox3.Text = result.ToString();
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            string firstValuetext = textBox1.Text;
+            double firstValue = Convert.ToDouble(firstValuetext);
+            string secondValuetext = textBox2.Text;
+            double secondValue = Convert.ToDouble(secondValuetext);
+            double result = secondValue - firstValue;
+            textBox3.Text = result.ToString();
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            string firstValuetext = textBox1.Text;
+            double firstValue = Convert.ToDouble(firstValuetext);
+            string secondValuetext = textBox2.Text;
+            double secondValue = Convert.ToDouble(secondValuetext);
+            double result = secondValue * firstValue;
+            textBox3.Text = result.ToString();
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            string firstValuetext = textBox1.Text;
+            double firstValue = Convert.ToDouble(firstValuetext);
+            string secondValuetext = textBox2.Text;
+            double secondValue = Convert.ToDouble(secondValuetext);
+            double result = secondValue / firstValue;
+            textBox3.Text = result.ToString();
+        }
     }
 }
